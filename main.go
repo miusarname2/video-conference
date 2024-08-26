a66b5c0e38de3df012aaa4e309c62c61d8ee3998
miusarname <oscarmauricio631@gmail.com>
2024-08-26 15:10:34 -0500

    go version

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+type Message struct {
+	Type    string `json:"type"`
+	Content string `json:"content"`
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer ws.Close()
+	clients[ws] = true
+
+	for {
+		var msg Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			fmt.Println(err)
+			delete(clients, ws)
+			break
+		}
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				fmt.Println(err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("index.html")
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+
+	fmt.Println("Server started on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server: ", err)
+	}
+}
---
+++ static/script.js
@@ -0,0 +1,81 @@
+const videoGrid = document.getElementById('video-grid');
+const peers = {};
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream);
+
+    const ws = new WebSocket('ws://' + window.location.host + '/ws');
+    ws.onmessage = ({ data }) => {
+        const message = JSON.parse(data);
+
+        switch (message.type) {
+            case 'offer':
+                handleOffer(message.content, ws, stream);
+                break;
+            case 'answer':
+                handleAnswer(message.content);
+                break;
+            case 'ice-candidate':
+                handleIceCandidate(message.content);
+                break;
+        }
+    };
+
+    ws.onopen = () => {
+        ws.send(JSON.stringify({ type: 'join', content: 'A user joined' }));
+    };
+});
+
+function handleOffer(offer, ws, stream) {
+    const peerConnection = new RTCPeerConnection();
+    peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+
+    stream.getTracks().forEach(track => {
+        peerConnection.addTrack(track, stream);
+    });
+
+    peerConnection.createAnswer().then(answer => {
+        peerConnection.setLocalDescription(answer);
+        ws.send(JSON.stringify({ type: 'answer', content: answer }));
+    });
+
+    peerConnection.onicecandidate = event => {
+        if (event.candidate) {
+            ws.send(JSON.stringify({ type: 'ice-candidate', content: event.candidate }));
+        }
+    };
+
+    peerConnection.ontrack = event => {
+        const video = document.createElement('video');
+        video.srcObject = event.streams[0];
+        video.addEventListener('loadedmetadata', () => {
+            video.play();
+        });
+        videoGrid.append(video);
+    };
+
+    peers[ws] = peerConnection;
+}
+
+function handleAnswer(answer) {
+    const peerConnection = peers[ws];
+    peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+}
+
+function handleIceCandidate(candidate) {
+    const peerConnection = peers[ws];
+    peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+}
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    });
+    videoGrid.append(video);
+}
